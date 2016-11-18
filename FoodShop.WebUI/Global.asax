f9e7afdd08979564c20d6bade08115e2426ea8e5
Stephen Shine <stephen.shine88@gmail.com>
2016-11-18 15:40:52 +0000

    Adding initial set up of FoodStore and NDR class

---
+++ FoodShop.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FoodShop.WebUI.MvcApplication" Language="C#" %>
---
+++ FoodShop.WebUI/Infrastructure/NinjectDR.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+
+namespace FoodShop.WebUI.Infrastructure
+{
+    public class NinjectDR : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDR(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            // Put bindings here
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+    }
+}

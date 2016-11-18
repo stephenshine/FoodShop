29b7a149c23c71f3780500401d273347154b7e68
Stephen Shine <stephen.shine88@gmail.com>
2016-11-18 20:44:11 +0000

    adding route for paging info.

--- FoodShop.WebUI/App_Start/RouteConfig.cs
+++ FoodShop.WebUI/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace FoodShop.WebUI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: null,
+                url: "Page{page}",
+                defaults: new { controller = "Product", action = "Index" }
+                );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Product", action = "Index", id = UrlParameter.Optional }

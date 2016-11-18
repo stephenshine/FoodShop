49f290c54d9d375b4203b20cce17cd0ed1a3e886
Stephen Shine <stephen.shine88@gmail.com>
2016-11-18 15:50:35 +0000

    Adding product class and repo abstract

---
+++ FoodShop.Domain/Abstract/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FoodShop.Domain.Entities;
+
+namespace FoodShop.Domain.Abstract
+{
+    class IProductRepository
+    {
+        IEnumerable<Product> Products { get; }
+    }
+}

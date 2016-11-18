c3d23c92ea0cecdf4451eba49fd660facddd139a
Stephen Shine <stephen.shine88@gmail.com>
2016-11-18 16:08:00 +0000

    Adding product controller

---
+++ FoodShop.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using FoodShop.Domain.Abstract;
+using FoodShop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodShop.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            repository = productRepository;
+        }
+
+        public ActionResult Index()
+        {
+            return View(repository.Products);
+        }
+    }
+}

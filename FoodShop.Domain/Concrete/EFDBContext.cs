c8d3521984353680cb057128716960060219311c
Stephen Shine <stephen.shine88@gmail.com>
2016-11-18 18:46:13 +0000

    Adding connection string to web.config file. Adding EFPrepo class.

---
+++ FoodShop.Domain/Concrete/EFDBContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using FoodShop.Domain.Entities;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodShop.Domain.Concrete
+{
+    public class EFDBContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ FoodShop.Domain/Concrete/EFProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FoodShop.Domain.Abstract;
+using FoodShop.Domain.Entities;
+
+namespace FoodShop.Domain.Concrete
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private EFDBContext context = new EFDBContext();
+
+        public IEnumerable<Product> Products
+        {
+            get { return context.Products; }
+        }
+    }
+}

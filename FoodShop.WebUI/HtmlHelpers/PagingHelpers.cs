d206255c04ae9ebb78d7c2fefd9fa5d24bb25f48
Stephen Shine <stephen.shine88@gmail.com>
2016-11-18 19:31:29 +0000

    Adding PagingInfo view model and PagingHelper.

---
+++ FoodShop.WebUI/HtmlHelpers/PagingHelpers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Web.Mvc;
+using FoodShop.WebUI.Models;
+
+namespace FoodShop.WebUI.HtmlHelpers
+{
+    public static class PagingHelpers
+    {
+        /// <summary>
+        /// HTML Helper to create paging info
+        /// </summary>
+        /// <param name="html"></param>
+        /// <param name="pagingInfo"></param>
+        /// <param name="pageUrl"></param>
+        /// <returns></returns>
+        public static MvcHtmlString PageLinks(this HtmlHelper html, PagingInfo pagingInfo, Func<int, string> pageUrl)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 1; i <= pagingInfo.TotalPages; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                tag.MergeAttribute("href", pageUrl(i));
+                tag.InnerHtml = i.ToString();
+                if (i == pagingInfo.CurrentPage)
+                {
+                    tag.AddCssClass("selected");
+                    tag.AddCssClass("btn-primary");
+                }
+                tag.AddCssClass("btn btn-default");
+                result.Append(tag.ToString());
+            }
+
+            return MvcHtmlString.Create(result.ToString());
+        }
+    }
+}
